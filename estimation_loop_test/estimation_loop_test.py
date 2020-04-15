da81383d5677fd93160bc0ca6ce4b8a2a9f3aa1e
ragibson <ryanalexandergibson@gmail.com>
2020-04-15 14:27:13 -0400

    Add the loop detection code to the estimation loop test

--- estimation_loop_test/estimation_loop_test.py
+++ estimation_loop_test/estimation_loop_test.py
@@ -1,6 +1,5 @@
 import igraph as ig
 import matplotlib.pyplot as plt
-import louvain
 import numpy as np
 from modularitypruning.champ_utilities import CHAMP_2D
 from modularitypruning.louvain_utilities import singlelayer_louvain
@@ -9,68 +8,76 @@ from modularitypruning.plotting import plot_estimates
 from modularitypruning.progress import Progress
 import sys
 
-NUM_TRIALS = 3
-progress = Progress(NUM_TRIALS)
+if __name__ == "__main__":
+    NUM_TRIALS = 10 ** 6
+    progress = Progress(NUM_TRIALS)
+    num_trials_completed = 0
 
-for i in range(NUM_TRIALS):
-    progress.increment()
+    for i in range(NUM_TRIALS):
+        progress.increment()
 
-    GAMMA_START = 0
-    GAMMA_END = 3
-    GAMMA_ITERS = 1000
+        GAMMA_START = 0
+        GAMMA_END = 3
+        GAMMA_ITERS = 1000
 
-    n = np.random.randint(10, 100)
-    m = np.random.randint(3 * n, 10 * n)
-    G = ig.Graph.Erdos_Renyi(n=n, m=m, directed=False)
+        n = np.random.randint(10, 100)
+        m = np.random.randint(3 * n, 10 * n)
+        try:
+            G = ig.Graph.Erdos_Renyi(n=n, m=m, directed=False)
+        except:
+            continue
 
-    if not G.is_connected():
-        continue
+        if not G.is_connected():
+            continue
 
-    gammas = np.linspace(GAMMA_START, GAMMA_END, GAMMA_ITERS)
-    all_parts = [singlelayer_louvain(G, g) for g in gammas]
+        gammas = np.linspace(GAMMA_START, GAMMA_END, GAMMA_ITERS)
+        all_parts = [singlelayer_louvain(G, g) for g in gammas]
 
-    if len(all_parts) == 0:
-        continue
+        if len(all_parts) == 0:
+            continue
 
-    # Print details on the CHAMP set when the number of communities is not restricted
-    ranges = CHAMP_2D(G, all_parts, GAMMA_START, GAMMA_END)
-    gamma_estimates = ranges_to_gamma_estimates(G, ranges)
+        # Print details on the CHAMP set when the number of communities is not restricted
+        ranges = CHAMP_2D(G, all_parts, GAMMA_START, GAMMA_END, single_threaded=True)
+        gamma_estimates = ranges_to_gamma_estimates(G, ranges)
 
-    for i in range(len(gamma_estimates)):
-        for j in range(len(gamma_estimates)):
-            if i == j:
-                continue
 
-            start1, end1, membership1, estimate1 = gamma_estimates[i]
-            start2, end2, membership2, estimate2 = gamma_estimates[j]
+        def gamma_to_domain(gamma):
+            for gamma_start, gamma_end, part, gamma_est in gamma_estimates:
+                if gamma_start <= gamma <= gamma_end:
+                    return part, gamma_est
+            return None, None
 
-            if estimate1 is None or estimate2 is None:
-                continue
 
-            if start1 <= estimate2 <= end1 and start2 <= estimate1 <= end2:
-                # if num_communities(membership1) > 8 or num_communities(membership2) > 8:
-                #     continue
+        for i in range(len(gamma_estimates)):
+            _, _, old_membership, old_estimate = gamma_estimates[i]
 
-                progress.done()
-                print(f"n={n}, m={m}")
+            for update_iteration in range(1000):
+                if old_estimate is None:
+                    break
 
-                print([(e.source, e.target) for e in G.es])
+                new_membership, new_estimate = gamma_to_domain(old_estimate)
+                if old_membership == new_membership:
+                    break
 
-                layout = G.layout_fruchterman_reingold(niter=1000)
-                ig.plot(louvain.RBConfigurationVertexPartition(G, initial_membership=membership1),
-                        bbox=(500, 500), layout=layout)
-                ig.plot(louvain.RBConfigurationVertexPartition(G, initial_membership=membership2),
-                        bbox=(500, 500), layout=layout)
-
+                old_membership, old_estimate = new_membership, new_estimate
+            else:
+                print(f"loop detected with n={n}, m={m}")
                 plot_estimates(gamma_estimates)
                 plt.show()
-
+                progress.done()
                 sys.exit(0)
 
-    plt.close()
-    plot_estimates(gamma_estimates)
-    plt.title(f"$G(n={n}, m={m})$ Realization Domains and Estimates")
-    plt.xlabel("$\gamma$", fontsize=14)
-    plt.ylabel("Number of communities", fontsize=14)
-    plt.xlim([-0.1, 3.1])
-    plt.savefig(f"G({n},{m}).pdf")
+        num_trials_completed += 1
+
+        # plt.close()
+        # plt.rc('text', usetex=True)
+        # plt.rc('font', family='serif')
+        # plot_estimates(gamma_estimates)
+        # plt.title(f"$G(n={n}, m={m})$ Realization Domains and Estimates")
+        # plt.xlabel("$\gamma$", fontsize=14)
+        # plt.ylabel("Number of communities", fontsize=14)
+        # plt.xlim([-0.1, 3.1])
+        # plt.savefig(f"G({n},{m}).pdf")
+
+    progress.done()
+    print(f"Actually completed {num_trials_completed} trials (out of {NUM_TRIALS})")
