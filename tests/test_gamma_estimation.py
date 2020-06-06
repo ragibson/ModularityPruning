180fd35afa09c71449bcb67565902877d426e498
ragibson <ryan.alexander.gibson@gmail.com>
2020-06-06 15:57:17 -0400

    Add test to check gamma estimation consistency on directed graphs

--- tests/test_gamma_estimation.py
+++ tests/test_gamma_estimation.py
@@ -2,7 +2,8 @@ import igraph as ig
 from math import log
 from numpy import mean
 from modularitypruning.parameter_estimation import iterative_monolayer_resolution_parameter_estimation
-from random import seed
+from modularitypruning.parameter_estimation_utilities import gamma_estimate
+from random import randint, seed
 import unittest
 
 
@@ -32,6 +33,28 @@ class TestMonolayerParameterEstimation(unittest.TestCase):
 
             # check we converged close to the ground truth "correct" value
             self.assertLess(abs(true_gamma - gamma), 0.05)
+
+    def test_directed_consistency_igraph_famous(self):
+        """Test consistency of gamma estimates on undirected and (symmetric) directed versions of various famous graphs.
+
+        In particular, we use
+            Meredith (n=70, m=140): a counterexample to a conjecture regarding 4-regular 4-connected Hamiltonian graphs
+            Nonline (n=50, m=72): a disconnnected graph composed of the 9 subgraphs whose presence makes a nonline graph
+            Thomassen (n=34, m=52): the smallest graph without a Hamiltonian path
+            Tutte (n=46, m=69): a counterexample to a conjecture regarding 3-connected 3-regular Hamiltonian graphs
+            Zachary (n=34, m=78): popular network of the social interactions between 34 members of a karate club
+        """
+
+        seed(0)
+        for name in ['meredith', 'nonline', 'thomassen', 'tutte', 'zachary']:
+            G = ig.Graph.Famous(name)
+            random_membership = [randint(0, 5) for _ in range(G.vcount())]
+
+            gamma_undirected = gamma_estimate(G, random_membership)
+            G.to_directed()
+            gamma_directed = gamma_estimate(G, random_membership)
+
+            self.assertAlmostEqual(gamma_undirected, gamma_directed, places=10)
 
 
 if __name__ == "__main__":
