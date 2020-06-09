b28047cf8727c7bb2d9ef3d5bbbfdbae49eb1504
ragibson <ryan.alexander.gibson@gmail.com>
2020-06-08 21:48:30 -0400

    Add alternate P_hat calculation to CHAMP 3D coefficient tests
    Importantly, this computation does not depend on multilayer louvain partitions

--- tests/test_champ_coefficients_3D.py
+++ tests/test_champ_coefficients_3D.py
@@ -1,7 +1,7 @@
 from shared_testing_functions import generate_connected_multilayer_ER, generate_random_partitions
 from modularitypruning.champ_utilities import partition_coefficients_3D
 from modularitypruning.louvain_utilities import multilayer_louvain_part_with_membership, \
-    check_multilayer_louvain_capabilities
+    check_multilayer_louvain_capabilities, louvain_part_with_membership
 from random import seed
 import unittest
 
@@ -38,6 +38,18 @@ class TestCHAMPCoefficients3D(unittest.TestCase):
             self.assertAlmostEqual(A_hat, louvain_A_hat, places=10)
             self.assertAlmostEqual(P_hat, louvain_P_hat, places=10)
             self.assertAlmostEqual(C_hat, louvain_C_hat, places=10)
+
+            # Also test against an alternate, per-layer calculation of P_hat
+            alternate_P_hat = 0
+            for layer in set(layer_membership):
+                this_layer_indices = [i for i, l in enumerate(layer_membership) if layer == l]
+                layer_subgraph = G_intralayer.subgraph(this_layer_indices)
+                layer_community_membership = [community for i, community in enumerate(membership)
+                                              if layer_membership[i] == layer]
+                layer_part = louvain_part_with_membership(layer_subgraph, layer_community_membership)
+                alternate_P_hat += (layer_part.quality(resolution_parameter=0.0) -
+                                    layer_part.quality(resolution_parameter=1.0))
+            self.assertAlmostEqual(alternate_P_hat, P_hat, places=10)
 
     def assert_partition_coefficient_correctness_unweighted_ER(self, num_nodes_per_layer, m, num_layers, directed,
                                                                num_partitions, K_max):
