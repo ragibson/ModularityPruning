b2e7d56fed7b4b905ec5729ee213fc014ee2e64c
Ryan Gibson <ryan.alexander.gibson@gmail.com>
2023-11-15 22:30:13 -0500

    Tweak documentation integration/webhook

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../utilities'))
 add_module_names = False
@@ -25,15 +26,14 @@ copyright = '2020, Ryan Gibson'
 author = 'Ryan Gibson'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.0'
-
+release = '1.3.5'
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.napoleon']
+extensions = ['sphinx.ext.napoleon', 'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -42,7 +42,6 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 
 # -- Options for HTML output -------------------------------------------------
 
